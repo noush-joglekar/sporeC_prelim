7c52adda8d0af7b9bac547b1adee56790be55fa1
Anoushka Joglekar <ajoglekar@nygenome.org>
2023-08-22 09:34:24 -0400

    full prom run. chromunity would be madness without the split input

---
+++ scripts/bashScripts/splitIntoChr_PromethION_PoreC_combinedFrags_slurm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --partition=pe2   # cluster-specific
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --job-name=prometh_splitChr
+#SBATCH --time=4:00:00   # HH/MM/SS
+#SBATCH --mem=16G   # memory requested, units available: K,M,G,T
+#SBATCH --mail-user=ajoglekar@nygenome.org
+#SBATCH --mail-type=ALL
+
+echo "Starting at:" `date` >> prometh_splitChr_preprocess.log
+
+echo "This is job number:" $SLURM_JOB_ID >> prometh_splitChr_preprocess.log
+echo "Running on node:" `hostname` >> prometh_splitChr_preprocess.log
+echo "Running on cluster:" $SLURM_CLUSTER_NAME >> prometh_splitChr_preprocess.log
+echo "This job was assigned the temporary (local) directory:" $TMPDIR >> prometh_splitChr_preprocess.log
+
+chrID=$1;
+
+echo "Processing chr"$chrID
+
+awk -v chr=$chrID 'BEGIN{c=1; comm="zcat ../NlaIII_GM12878_output/batchCombined_fragFile_sorted_wClosestGene.bed.gz"; \
+while(comm|getline) {if($1~chr) {if(!id[$4]) {id[$4]=c; c=c+1;} $15=id[$4];
+OFS="\t"; print }} }' | gzip -c > NlaIII_GM12878_chr${chrID}.gz
+
+echo "Job finished or was terminated, please check logs"
+exit
