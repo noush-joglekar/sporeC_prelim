d465d11b75088447f0e915f4c3242313e1e37086
Anoushka Joglekar <ajoglekar@nygenome.org>
2023-09-06 10:37:15 -0400

    made changes to incorporate primary and secondary distance cutoffs

--- scripts/bashScripts/makeProjMat.sh
+++ scripts/bashScripts/makeProjMat.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 #SBATCH --partition=pe2   # cluster-specific
-#SBATCH --array=1-10000%100
+#SBATCH --array=1-10000%500
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=4
 #SBATCH --job-name=arrPM
-#SBATCH --time=00:30:00   # HH/MM/SS
-#SBATCH --mem=1G   # memory requested, units available: K,M,G,T
+#SBATCH --time=00:05:30   # HH/MM/SS
+#SBATCH --mem=500M   # memory requested, units available: K,M,G,T
 #SBATCH --mail-user=ajoglekar@nygenome.org
 #SBATCH --mail-type=ALL
 
@@ -24,7 +24,8 @@ echo "Processing "$SLURM_ARRAY_TASK_ID
 
 ## IMPORTANT: Directories hard coded in python script
 
-python /gpfs/commons/groups/gursoy_lab/ajoglekar/Projects/2023_03_01_multiwayInteractions/v0.analysis/scripts/pythonScripts/makeProjectionMatrices.py $SLURM_ARRAY_TASK_ID 600
+python /gpfs/commons/groups/gursoy_lab/ajoglekar/Projects/2023_03_01_multiwayInteractions/v0.analysis/scripts/pythonScripts/makeProjectionMatrices.py \
+chains_10k_500_projectionMtxOutput/ $SLURM_ARRAY_TASK_ID 600 1000 4 --offDiagLim 3
 
 echo "Job finished or was terminated, please check logs"
 exit
