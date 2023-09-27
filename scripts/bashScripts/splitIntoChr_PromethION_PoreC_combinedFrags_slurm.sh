3ca05b14a815d0f53172a29b1541fb7cc3ad5c83
Anoushka Joglekar <ajoglekar@nygenome.org>
2023-09-27 18:51:15 -0400

    was soft matching chromosomes i.e. it included chr1 and chr6 in chr16 etc.

--- scripts/bashScripts/splitIntoChr_PromethION_PoreC_combinedFrags_slurm.sh
+++ scripts/bashScripts/splitIntoChr_PromethION_PoreC_combinedFrags_slurm.sh
@@ -4,27 +4,28 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --job-name=prometh_splitChr
-#SBATCH --time=4:00:00   # HH/MM/SS
-#SBATCH --mem=16G   # memory requested, units available: K,M,G,T
+#SBATCH --job-name=splitChr_prom
+#SBATCH --time=00:30:00   # HH/MM/SS
+#SBATCH --mem=5G   # memory requested, units available: K,M,G,T
 #SBATCH --mail-user=ajoglekar@nygenome.org
-#SBATCH --mail-type=ALL
+#SBATCH --mail-type=FAIL
 
-echo "Starting at:" `date` >> prometh_splitChr_preprocess.log
+echo "Starting at:" `date` >> splitChr_prom.log
 
-echo "This is job number:" $SLURM_JOB_ID >> prometh_splitChr_preprocess.log
-echo "Running on node:" `hostname` >> prometh_splitChr_preprocess.log
-echo "Running on cluster:" $SLURM_CLUSTER_NAME >> prometh_splitChr_preprocess.log
-echo "This job was assigned the temporary (local) directory:" $TMPDIR >> prometh_splitChr_preprocess.log
+echo "This is job number:" $SLURM_JOB_ID >> splitChr_prom.log
+echo "Running on node:" `hostname` >> splitChr_prom.log
+echo "Running on cluster:" $SLURM_CLUSTER_NAME >> splitChr_prom.log
+echo "This job was assigned the temporary (local) directory:" $TMPDIR >> splitChr_prom.log
 
 chrID=$1;
 file=$2;
+prefix=$3;
 
 echo "Processing chr"$chrID
 
-awk -v chr=$chrID -v file=$file 'BEGIN{c=1; comm="zcat "file; \
-while(comm|getline) {if($1~chr) {if(!id[$4]) {id[$4]=c; c=c+1;} $15=id[$4];
-OFS="\t"; print }} }' | gzip -c > NlaIII_GM12878_chr${chrID}.gz
+awk -v chr="chr"${chrID} -v file=$file 'BEGIN{c=1; comm="zcat "file; \
+while(comm|getline) {if($1==chr) {if(!id[$4]) {id[$4]=c; c=c+1;} $15=id[$4];
+OFS="\t"; print }} }' | gzip -c > ${prefix}_chr${chrID}.gz
 
 echo "Job finished or was terminated, please check logs"
 exit
