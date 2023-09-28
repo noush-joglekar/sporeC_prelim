4ab5f6f05278bf86114a0d0dce6d83accca2f957
Anoushka Joglekar <ajoglekar@nygenome.org>
2023-09-28 09:05:26 -0400

    added off diag dist as a parameter

--- scripts/bashScripts/makeHypergraphDict_from_bIncDFs.sh
+++ scripts/bashScripts/makeHypergraphDict_from_bIncDFs.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --job-name=HPDict
-#SBATCH --time=48:00:00   # HH/MM/SS
+#SBATCH --time=24:00:00   # HH/MM/SS
 #SBATCH --mem=4G   # memory requested, units available: K,M,G,T
 #SBATCH --mail-user=ajoglekar@nygenome.org
 #SBATCH --mail-type=ALL
@@ -20,11 +20,12 @@ echo "This job was assigned the temporary (local) directory:" $TMPDIR >> hpDict.
 source activate hypergraph_poreC
 
 numFiles=$1;
+offDiagDist=$2;
 ## IMPORTANT: Directories hard coded in python script
 
 time python \
 /gpfs/commons/groups/gursoy_lab/ajoglekar/Projects/2023_03_01_multiwayInteractions/v0.analysis/scripts/pythonScripts/makeHypergraphDict_fromChainIncDFs.py \
-$numFiles
+$numFiles $offDiagDist
 
 echo "Job finished or was terminated, please check logs"
 exit
