43857f49202d5ee2523c660c268f83fb3ba78037
Anoushka Joglekar <ajoglekar@nygenome.org>
2023-09-28 09:00:04 -0400

    added argument for off diagonal distance

--- scripts/pythonScripts/makeHypergraphDict_fromChainIncDFs.py
+++ scripts/pythonScripts/makeHypergraphDict_fromChainIncDFs.py
@@ -12,12 +12,15 @@ from chains import dfToDict, dictToDF
 ## Set up.
 dataDir = '/gpfs/commons/groups/gursoy_lab/ajoglekar/Projects/2023_03_01_multiwayInteractions/2023_03_01_v0_dataGathering/v0_hypergraphSimulations/getMultiwayInteractions_fromBPChains/'
 
+numFiles = int(sys.argv[1])
+offDiagDist = sys.argv[2]
+
 def constructFullDict(numFiles):
     """Takes in a directory of DFs and outputs a dict"""
     result_dict = {}
     numEdges = []
     for ix in range(1,numFiles+1):
-        filePath = f'{dataDir}chains_10k_500_projectionMtxOutput/binConcatInc_3_600_750_{ix}.pkl'
+        filePath = f'{dataDir}chains_10k_500_projectionMtxOutput/binConcatInc_{offDiagDist}_600_750_{ix}.pkl'
         if os.path.isfile(filePath):
             bIncDF = pd.read_pickle(filePath)
             result_dict = dfToDict(bIncDF,result_dict)
@@ -25,7 +28,6 @@ def constructFullDict(numFiles):
             numEdges.append(nE)
     return(result_dict,numEdges)
 
-numFiles = int(sys.argv[1])
 
 print("About to read in",numFiles,"files")
 
@@ -33,8 +35,8 @@ hpEdges, numEdges = constructFullDict(numFiles)
 
 print("Constructed hyperedge dict - writing output")
 
-with open(f'{dataDir}hyperEdges_3_600_750_{numFiles}_chains.pkl', 'wb') as f:
+with open(f'{dataDir}hyperEdges_{offDiagDist}_600_750_{numFiles}_chains.pkl', 'wb') as f:
     data = pickle.dump(hpEdges,f)
 
-numEdgesPath = f'{dataDir}numEdges_3_600_750_{numFiles}_chains.txt'
+numEdgesPath = f'{dataDir}numEdges_{offDiagDist}_600_750_{numFiles}_chains.txt'
 np.savetxt(numEdgesPath, numEdges, delimiter='\t', fmt='%d')
