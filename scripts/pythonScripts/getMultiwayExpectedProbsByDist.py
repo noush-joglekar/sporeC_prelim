d9c6458109a19329b9412509571626ea88ed864d
Anoushka Joglekar <ajoglekar@nygenome.org>
2023-09-29 11:39:40 -0400

    My boolean input specification was weird. Fixed it

--- scripts/pythonScripts/getMultiwayExpectedProbsByDist.py
+++ scripts/pythonScripts/getMultiwayExpectedProbsByDist.py
@@ -5,6 +5,8 @@ import numpy as np
 import pickle
 import warnings
 import argparse
+import os
+import json
 
 import sys
 sys.path.append('/gpfs/commons/groups/gursoy_lab/ajoglekar/Projects/2023_03_01_multiwayInteractions/v0.analysis/scripts/pythonScripts/functions/')
@@ -18,12 +20,18 @@ def main():
     workingDir = args.workingDir
     plotDir = f'{dataDir}{workingDir}{args.plotDir}/'
     outDir = f'{dataDir}{workingDir}{args.outDir}/'
+    probHashOutName = f'{outDir}/{args.probHashOutFile}.json'
     seed = args.seed
     toChoose = args.sampleSize
-    toPlotRef = args.toPlotRef
-    toPlotInd = args.toPlotInd
-    toPlotScatter = args.toPlotScatter
+    print("Input args ------ toChoose:",toChoose)
+    toPlotRef = args.plotRef
+    print("Input args ------ toPlotRef:",toPlotRef)
+    toPlotInd = args.plotInd
+    print("Input args ------ toPlotInd:",toPlotInd)
+    toPlotScatter = args.plotScatter
+    print("Input args ------ toPlotScatter:",toPlotScatter)
 
+    print("Loading in hyperedge pickle file")
     with open(f'{dataDir}/{inputPkl}','rb') as f:
         hpEdges = pickle.load(f)
 
@@ -34,10 +42,22 @@ def main():
     evalInstance = multiwayEval(keyCard, hpEdges, hpKeys_split, seed, 
                                 toChoose, toPlotRef, toPlotInd, toPlotScatter,
                                 plotDir,outDir)
-    probHash = multiwayEval.makeAllReferenceHashDicts()
+    
+    if os.path.exists(probHashOutName):
+        print("Expected probabilities file already exists...moving on")
+        with open(probHashOutName,'r') as file:
+            tmpHash = json.load(file)
+            probHash = {key: {int(k): v for k, v in value.items()} 
+                        for key, value in tmpHash.items()}
+    else:
+        print("Expected probabilities file does not exist...creating now")
+        probHash = evalInstance.makeAllReferenceHashDicts()
+        with open(probHashOutName,'w') as file:
+            json.dump(probHash,file)
+        print("File created...moving on")
     
     warnings.filterwarnings('ignore')
-    evalInstance.statsForAllReads(self,probHash)
+    evalInstance.statsForAllReads(probHash)
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -51,11 +71,12 @@ def parse_args():
     parser.add_argument("plotDir",type=str, help="Plot directory relative to working dir")
     parser.add_argument("outDir",type=str, help="Output directory relative to working dir")
     parser.add_argument("inputPkl", type=str, help="Input pickle file relative to data dir containing reads as hyperedges")
+    parser.add_argument("probHashOutFile", type=str, help="Name of probability reference hash")
     parser.add_argument("seed", type=int, help="Seed for read sampling")
     parser.add_argument("sampleSize", type=int, help="Number of reads to sample")
-    parser.add_argument("toPlotRef", type=bool, help="Plot dist stratified probabilities per card and subset",default=True)
-    parser.add_argument("toPlotInd", type=bool, help="Plot exp vs. obs of individual sampled reads", default=False)
-    parser.add_argument("toPlotScatter", type=bool, help="Plot scatterplot of avg similarity measures for sampled reads",default=False)
+    parser.add_argument("--plotRef", action="store_true", help="Specify is you want a hist of dist stratified probabilities per card and subset")
+    parser.add_argument("--plotInd", action="store_true", help="Specificy if you want an exp vs. obs of individual sampled reads")
+    parser.add_argument("--plotScatter", action="store_true", help="Specify if you want a scatterplot of avg similarity measures for sampled reads")
 
     return parser.parse_args()
 
