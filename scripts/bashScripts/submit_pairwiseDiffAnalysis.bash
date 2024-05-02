6e5eb4c87abc1809b6407c3577924bfde2ded01b
Anoushka Joglekar <ajoglekar@nygenome.org>
2024-05-02 15:06:52 -0400

    bash scripts for slurm

---
+++ scripts/bashScripts/submit_pairwiseDiffAnalysis.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Submitting for HCC versus HG"
+for card in 3 4 5 ; do echo $card; sbatch \
+/gpfs/commons/groups/gursoy_lab/ajoglekar/Projects/2023_03_01_multiwayInteractions/v0.analysis/scripts/bashScripts/v1.runDifferentialAnalysis_slurm.sh \
+differentialAnalysis/hcc_vs_hg/ HCC1854 HG002 $card ; done
+
+echo "Submitting for HCC versus GM"
+for card in 3 4 5 ; do echo $card; sbatch \
+/gpfs/commons/groups/gursoy_lab/ajoglekar/Projects/2023_03_01_multiwayInteractions/v0.analysis/scripts/bashScripts/v1.runDifferentialAnalysis_slurm.sh \
+differentialAnalysis/gm_vs_hcc/ HCC1854 GM12878 $card ; done
+
