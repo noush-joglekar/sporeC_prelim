4c74941199fa24cf3eb589a298a4fe35ce726265
Anoushka Joglekar <ajoglekar@nygenome.org>
2024-05-14 12:38:11 -0400

    passed "$" to function. Made file names unambiguous

--- scripts/bashScripts/getGeneNamesFromChromunities_LNCaP.bash
+++ scripts/bashScripts/getGeneNamesFromChromunities_LNCaP.bash
@@ -24,19 +24,19 @@ if [ ! -d "$outputDir" ]; then
 mkdir $outputDir
 fi
 
-zcat $fragFile | awk -v batch=$batch '{FS=","; if($6!=current) {id++; current=$6;} \
+zcat $fragFile | awk -v batch=$batch '{FS=","; OFS="\t"; if($6!=current) {id++; current=$6;} \
 if($7=="True" && !($2~"_|EBV") ) {count[id]++; \
-if(coord[id]){coord[id]=coord[$id]"\n"$2"\t"$3"\t"$4"\tRead:"id}else{coord[id]=$2"\t"$3"\t"$4"\tRead:"id}} }END \
+if(coord[id]){coord[id]=coord[id]"\n"$2"\t"$3"\t"$4"\tRead:"id}else{coord[id]=$2"\t"$3"\t"$4"\tRead:"id}} }END \
 {for(c in count) {if(count[c]>=2) {split(coord[c],entries,"\n"); \
-for(en in entries) {print entries[en]"_Card:"count[c]"_Batch:"batch} }} }' > ${outputDir}/fragFile.bed
+for(en in entries) {print entries[en]"_Card:"count[c]"_Batch:"batch} }} }' > ${outputDir}/fragFile_${batch}.bed
 
-bedtools sort -i ${outputDir}/fragFile.bed > ${outputDir}/fragFile_sorted.bed
-bedtools closest -d -a ${outputDir}/fragFile_sorted.bed -b geneCoordsAndNames_sorted.bed | tr -d "\"|;" > ${outputDir}/fragFile_sorted_wClosestGene.bed
-cat ${outputDir}/fragFile_sorted_wClosestGene.bed | awk '!seen[$1"_"$2"_"$3"_"$4]++' > ${outputDir}/fragFile_sorted_wClosestGene_Batch${batch}.bed
+bedtools sort -i ${outputDir}/fragFile_${batch}.bed > ${outputDir}/fragFile_${batch}_sorted.bed
+bedtools closest -d -a ${outputDir}/fragFile_${batch}_sorted.bed -b geneCoordsAndNames_sorted.bed | tr -d "\"|;" > ${outputDir}//fragFile_sorted_wClosestGene_tmp_${batch}.bed
+cat ${outputDir}//fragFile_sorted_wClosestGene_tmp_${batch}.bed | awk '!seen[$1"_"$2"_"$3"_"$4]++' > ${outputDir}/fragFile_sorted_wClosestGene_Batch${batch}.bed
 gzip ${outputDir}/fragFile_sorted_wClosestGene_Batch${batch}.bed
 
 time zcat ${outputDir}/fragFile_sorted_wClosestGene_Batch${batch}.bed.gz | awk '{split($4,r,/:|_/); OFS="\t"; \
 print r[5]":"r[6]"_"r[1]":"r[2],r[4],$9,$10,$11,$11}' | \
 gzip -c > ${outputDir}/readChromunities_wClosestGene_Batch${batch}.gz
 
-rm ${outputDir}/fragFile.bed ${outputDir}/fragFile_sorted.bed ${outputDir}/fragFile_sorted_wClosestGene.bed
+rm ${outputDir}/fragFile_${batch}.bed ${outputDir}/fragFile_${batch}_sorted.bed ${outputDir}//fragFile_sorted_wClosestGene_tmp_${batch}.bed
---
+++ scripts/bashScripts/getGeneNamesFromChromunities_hiPoreC.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+# By Anoushka Joglekar
+# 05.2024
+
+## Yet another format -__-
+
+module load bedtools
+
+fragFile=$1;
+batch=$2;
+outputDir=$3;
+
+if [ ! -d "$outputDir" ]; then
+mkdir $outputDir
+fi
+
+zcat $fragFile | awk -v batch=$batch '{FS=","; if($1!=current) {id++; current=$1;} \
+if($16=="FirstFilter" && !($2~"_|EBV") ) {count[id]++; \
+if(coord[id]){coord[id]=coord[id]"\n"$6"\t"$8"\t"$9"\tRead:"id}else{coord[id]=$6"\t"$8"\t"$9"\tRead:"id}} }END \
+{for(c in count) {if(count[c]>=2) {split(coord[c],entries,"\n"); \
+for(en in entries) {print entries[en]"_Card:"count[c]"_Batch:"batch} }} }' > ${outputDir}/fragFile_${batch}.bed
+
+bedtools sort -i ${outputDir}/fragFile_${batch}.bed > ${outputDir}/fragFile_${batch}_sorted.bed
+bedtools closest -d -a ${outputDir}/fragFile_${batch}_sorted.bed -b geneCoordsAndNames_sorted.bed | tr -d "\"|;" > ${outputDir}/fragFile_sorted_wClosestGene_tmp_${batch}.bed
+
+cat ${outputDir}/fragFile_sorted_wClosestGene_tmp_${batch}.bed | awk '!seen[$1"_"$2"_"$3"_"$4]++' > ${outputDir}/fragFile_sorted_wClosestGene_Batch${batch}.bed
+gzip ${outputDir}/fragFile_sorted_wClosestGene_Batch${batch}.bed
+
+time zcat ${outputDir}/fragFile_sorted_wClosestGene_Batch${batch}.bed.gz | awk '{split($4,r,/:|_/); OFS="\t"; \
+print r[5]":"r[6]"_"r[1]":"r[2],r[4],$9,$10,$11,$11}' | \
+gzip -c > ${outputDir}/readChromunities_wClosestGene_Batch${batch}.gz
+
+rm ${outputDir}/fragFile_${batch}.bed ${outputDir}/fragFile_${batch}_sorted.bed ${outputDir}/fragFile_sorted_wClosestGene_tmp_${batch}.bed
