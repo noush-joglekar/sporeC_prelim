164f7094e6b0a13db567e6c180d1faaaecb9459e
Anoushka Joglekar <ajoglekar@nygenome.org>
2024-05-29 11:35:19 -0400

    processing the hi-porec data. different file format + restriction enzyme

--- scripts/bashScripts/getGeneNamesFromChromunities_hiPoreC.bash
+++ scripts/bashScripts/getGeneNamesFromChromunities_hiPoreC.bash
@@ -15,7 +15,7 @@ mkdir $outputDir
 fi
 
 zcat $fragFile | awk -v batch=$batch '{FS=","; if($1!=current) {id++; current=$1;} \
-if($16=="FirstFilter" && !($2~"_|EBV") ) {count[id]++; \
+if(!($2~"_|EBV") ) {count[id]++; \
 if(coord[id]){coord[id]=coord[id]"\n"$6"\t"$8"\t"$9"\tRead:"id}else{coord[id]=$6"\t"$8"\t"$9"\tRead:"id}} }END \
 {for(c in count) {if(count[c]>=2) {split(coord[c],entries,"\n"); \
 for(en in entries) {print entries[en]"_Card:"count[c]"_Batch:"batch} }} }' > ${outputDir}/fragFile_${batch}.bed
@@ -30,4 +30,4 @@ time zcat ${outputDir}/fragFile_sorted_wClosestGene_Batch${batch}.bed.gz | awk '
 print r[5]":"r[6]"_"r[1]":"r[2],r[4],$9,$10,$11,$11}' | \
 gzip -c > ${outputDir}/readChromunities_wClosestGene_Batch${batch}.gz
 
-rm ${outputDir}/fragFile_${batch}.bed ${outputDir}/fragFile_${batch}_sorted.bed ${outputDir}/fragFile_sorted_wClosestGene_tmp_${batch}.bed
+#rm ${outputDir}/fragFile_${batch}.bed ${outputDir}/fragFile_${batch}_sorted.bed ${outputDir}/fragFile_sorted_wClosestGene_tmp_${batch}.bed
---
+++ scripts/bashScripts/submit_pairwiseDiffAnalysis_hiPoreC.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Submitting for HiPoreC cell lines"
+for card in 3 4 5 ; do echo $card; sbatch \
+/gpfs/commons/groups/gursoy_lab/ajoglekar/Projects/2023_03_01_multiwayInteractions/v0.analysis/scripts/bashScripts/v1.runDifferentialAnalysis_slurm_LNCaP.sh \
+differentialAnalysis/HiPoreC_cL/ K562 GM12878 $card ; done
